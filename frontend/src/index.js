b6f6bd2ca0e24827cd8f685166d586bf267cc596
leelasaikrishnarali <leelasaikrishna.rali@gmail.com>
2025-10-17 14:07:40 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/online-food-delivery">
+    <BrowserRouter basename="/food-delivery">
       <App />
     </BrowserRouter>
   </React.StrictMode>
